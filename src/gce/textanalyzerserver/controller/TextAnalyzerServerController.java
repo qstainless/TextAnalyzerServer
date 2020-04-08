9a3e0a43cf99421306fd8d01b25874e2c32f2634
Queue Stainless <gce517+git@gmail.com>
2020-04-08 15:16:56 -0400

    Display error message in console.

--- src/gce/textanalyzerserver/controller/TextAnalyzerServerController.java
+++ src/gce/textanalyzerserver/controller/TextAnalyzerServerController.java
@@ -33,6 +33,7 @@ public class TextAnalyzerServerController {
             // Extract the words from the URL content and insert them into the database
             Database.storeWordsIntoDatabase(targetHtmlContent);
         } catch (IOException e) {
+            System.out.println("Error: Invalid URL. Unable to process.");
             e.printStackTrace();
         }
     }
